614d8f2f72c683a532fdef44a1799ad985049244
UsmanDev09 <usiddique09@gmail.com>
2025-01-26 18:50:17 +0500

    Build issues resolved

--- apps/resume/next.config.ts
+++ apps/resume/next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname, 'src')
-    return config
-  },
+
 
   env: {
     DATABASE_URL: process.env.DATABASE_URL,
