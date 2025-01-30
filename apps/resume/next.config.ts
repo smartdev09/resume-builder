40b7a482aa8e10e0e38d056f7105b5bb5d835038
UsmanDev09 <usiddique09@gmail.com>
2025-01-30 14:57:04 +0500

    Build issues resolved

--- apps/resume/next.config.ts
+++ apps/resume/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
+  output: 'standalone',
+  outputFileTracingIncludes: {
+    '/apps/resume': ['../../packages/database/generated/**/*']
+  },
 
 
   env: {
